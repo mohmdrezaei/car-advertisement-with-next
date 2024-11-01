df00fb144aa5244f2f3e9f1746bd4e666a69e2b1
mohmdrezaei <mhwmdrz9@gmail.com>
2024-11-01 23:46:05 +0330

    Create Layout file

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -1,8 +1,15 @@
-import  styles from "./Layout.module.css"
+import styles from "./Layout.module.css"
 
-function Layout() {
+function Layout({children}) {
   return (
-    <div>Layout</div>
+     <>
+     <header className={styles.header}>
+        <h2>NextCar</h2>
+        <p>Choose an Buy your car</p>
+     </header >
+      <div className={styles.container}>{children}</div>
+     <footer className={styles.footer}>mohammad rezaei | next car &copy; </footer>
+     </>
   )
 }
 
