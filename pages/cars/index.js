8d507d0dc6a1acd600309689e6ca5a6ba894361f
mohmdrezaei <mhwmdrz9@gmail.com>
2024-11-02 16:38:11 +0330

    Add the cars page to the project

---
+++ pages/cars/index.js
@@ -0,0 +1,10 @@
+import CarsPage from "@/components/templates/CarsPage"
+import carsData from "@/data/carsData"
+
+function Details() {
+  return (
+    <CarsPage data={carsData} /> 
+  )
+}
+
+export default Details
